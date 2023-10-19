dca36ae79b4500fc92b9919cde1c5ed85ab72b62
stand4r <sahojko.dima2018@gmail.com>
2023-10-19 22:21:08 +0400

    add diassembler.py; start coding binary disassembler

--- file.py
+++ file.py
@@ -1,2 +1,11 @@
-for i in open("output.txt", "r").readlines():
-    print(i)
+a = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+/"
+print(len(a))
+x = {}
+f = open("output.exh", "wb")
+f2 = open("cz.txt", "rb").readlines()
+for i in range(64):
+    x[i] = a[i]
+
+for i in range(len(f2)):
+    f.write(x[int(f2[i], 16)])
+    print("end")
